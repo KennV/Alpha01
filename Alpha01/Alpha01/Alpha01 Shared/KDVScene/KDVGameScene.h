f70da9e9f4a690260ad572c00983f450b28de611
Kenn Villegas <kenn.villegas@Mac.Com>
2018-08-12 12:31:38 -0400

    ADDED MY SCENERY
    let's see if it works

---
+++ Alpha01/Alpha01/Alpha01 Shared/KDVScene/KDVGameScene.h
@@ -0,0 +1,13 @@
+//
+//  KDVGameScene.h
+//  Alpha01
+//
+//  Created by Kenn Villegas on 8/12/18.
+//  Copyright © 2018 Kenn Villegas. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface KDVGameScene : SKScene
+
+@end
---
+++ Alpha01/Alpha01/Alpha01 Shared/KDVScene/KDVGameScene.m
@@ -0,0 +1,13 @@
+//
+//  KDVGameScene.m
+//  Alpha01
+//
+//  Created by Kenn Villegas on 8/12/18.
+//  Copyright © 2018 Kenn Villegas. All rights reserved.
+//
+
+#import "KDVGameScene.h"
+
+@implementation KDVGameScene
+
+@end
