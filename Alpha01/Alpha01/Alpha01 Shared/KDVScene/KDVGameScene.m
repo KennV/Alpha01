3cb394c0b5d3d2322def2b643153304a6e11115d
Kenn Villegas <kenn.villegas@Mac.Com>
2018-08-13 02:37:57 -0400

    VOID

--- Alpha01/Alpha01/Alpha01 Shared/KDVScene/KDVGameScene.m
+++ Alpha01/Alpha01/Alpha01 Shared/KDVScene/KDVGameScene.m
@@ -6,8 +6,18 @@
 //  Copyright © 2018 Kenn Villegas. All rights reserved.
 //
 
+/**
+ So it looks as if I should make this file first
+ other rsrcs come from IB
+ */
+
+
 #import "KDVGameScene.h"
 
-@implementation KDVGameScene
+@implementation KDVGameScene {
+  SKShapeNode *_shapeNode01;
+  SKLabelNode *_labelNode;
+  SKShader *_shader;
+}
 
 @end
